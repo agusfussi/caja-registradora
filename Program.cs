c06099c2eb9be684a08742b690ab5f603c7be745
agusfussi <agusfussiwor@gmail.com>
2026-08-10 10:06:24 -0300

    etapa 2: carga de un producto

--- Program.cs
+++ Program.cs
@@ -3,3 +3,12 @@ Console.Write("Ingrese el nombre del cajero: ");
 string NombreCajero = Console.ReadLine();
 Console.WriteLine($"Bienvenido {NombreCajero}, a {NombreSuper}.");
 Console.ReadKey();
+
+Console.Write("Ingrese el nombre de un producto: ");
+string NombreProducto = Console.ReadLine();
+Console.Write("Escribe el precio del producto: ");
+string PrecioProducto = Console.ReadLine();
+Decimal precio = Convert.ToDecimal(PrecioProducto);
+
+Console.WriteLine($"El precio de {NombreProducto} es: {precio}");
+Console.ReadKey();
