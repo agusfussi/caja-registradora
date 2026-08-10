16bdd4ebd825cd0d365296e7ff23a5e2f94607cc
agusfussi <agusfussiwor@gmail.com>
2026-08-10 09:48:15 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string NombreSuper = "SuSuper";
+Console.Write("Ingrese el nombre del cajero: ");
+string NombreCajero = Console.ReadLine();
+Console.WriteLine($"Bienvenido {NombreCajero}, a {NombreSuper}.");
+Console.ReadKey();
